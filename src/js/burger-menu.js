60da90184cfbf430bd81457547c973c99e3e157e
IrynaSlavinska <133566139+IrynaSlavinska@users.noreply.github.com>
2023-10-26 20:01:35 +0300

    burger

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -6,7 +6,4 @@ burger.addEventListener('click', toggleMenu);
 function toggleMenu() {
   burger.classList.toggle('burger-active');
   burgerMenu.classList.toggle('burger-active');
-  burgerMenu.classList.contains('burger-active')
-    ? (document.body.style.overflow = 'hidden')
-    : (document.body.style.overflow = 'auto');
 }
