141518378a429a84a227430e98320763991c0efb
IrynaSlavinska <133566139+IrynaSlavinska@users.noreply.github.com>
2023-10-26 17:02:13 +0000

    Deploying to gh-pages from @ IrynaSlavinska/delivery@60da90184cfbf430bd81457547c973c99e3e157e 🚀

---
+++ index.d62a1465.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".header-burger"),t=document.querySelector(".header-menu");e.addEventListener("click",(function(){e.classList.toggle("burger-active"),t.classList.toggle("burger-active"),t.classList.contains("burger-active")?document.body.style.overflow="hidden":document.body.style.overflow="auto"}))}();
+//# sourceMappingURL=index.d62a1465.js.map
---
+++ index.ff5fad10.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".header-burger"),t=document.querySelector(".header-menu");e.addEventListener("click",(function(){e.classList.toggle("burger-active"),t.classList.toggle("burger-active"),t.classList.contains("burger-active")?document.body.style.overflow="hidden":document.body.style.overflow="auto"}));
+//# sourceMappingURL=index.ff5fad10.js.map
